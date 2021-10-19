acc51f51888ed4131a92315b368573291227334c
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-10-19 12:46:52 +0530

    refactor

--- block-BRaabk/code/food-menu/src/component/Menu.js
+++ block-BRaabk/code/food-menu/src/component/Menu.js
@@ -1,13 +1,23 @@
 function Menu(props) {
-    console.log(props)
-    return (
-        <>
-      <h1>
-        {props.data.map((e) => {
-        console.log(e.title);
+  return (
+    <>
+      <h1 className="container flex wrap">
+        {props.info.map((e) => {
           return (
             <>
-              <h2>{e.title}</h2>
+              <div className="flex-45 card">
+                <div>
+                  <img className="image-width" src={`${e.img}`} alt={e.title} />
+                </div>
+                <div>
+                  <div className="flex">
+                    <div className="title">{e.title.toUpperCase()}</div>
+                    <span>${e.price}</span>
+                  </div>
+                  <hr />
+                  <span>{e.desc.charAt().toUpperCase() + e.desc.slice(1)}</span>
+                </div>
+              </div>
             </>
           );
         })}
