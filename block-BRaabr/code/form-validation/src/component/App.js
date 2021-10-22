0a063e7a00b02a22b95343986492f5c31327ca91
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-10-22 15:17:59 +0530

    validation

---
+++ block-BRaabr/code/form-validation/src/component/App.js
@@ -0,0 +1,13 @@
+import MultipInput from "./MultipInput";
+import Validation from "./Validation";
+
+function App() {
+  return (
+    <>
+      <MultipInput />
+      <Validation/>
+    </>
+  );
+}
+
+export default App;
